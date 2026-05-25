b8073bb1f2d271b9853ebf2d1d8f1d70e9368f73
yosukeN <yousken72@gmail.com>
2026-05-25 14:33:19 +0900

    write update, and delete feature.

--- file-system/index.js
+++ file-system/index.js
@@ -1,6 +1,6 @@
 'use strict'
 const {extname} = require('path');
-const {readdir, readFile, writeFile} = require('fs').promises;
+const {readdir, readFile, writeFile, unlink} = require('fs').promises;
 
 exports.fetchAll = async () => {
     // Read all json-files in the same data directory
@@ -18,4 +18,26 @@ exports.fetchByCompleted = (completed) => exports.fetchAll()
 exports.create = (todo) => 
     writeFile(`${__dirname}/${todo.id}.json`, JSON.stringify(todo));
 
-// Implement update and delete functions as needed
+exports.update = async (id, updates) => {
+    const fileName = `${__dirname}/${id}.json`;
+    return readFile(fileName, 'utf8').then(
+        content => {
+            const todo = {
+                ...JSON.parse(content),
+                ...updates
+            };
+            return writeFile(fileName, JSON.stringify(todo)).then(() => todo);
+        },
+        // If the file doesn't exist, return null, otherwise propagate the error
+        err => err.code === 'ENOENT' ? null : Promise.reject(err)
+    );
+};
+
+exports.delete = (id) => {
+    unlink(`${__dirname}/${id}.json}`)
+    .then(
+        () => id,
+        // if the file doesn't exist, return null, otherwise propagate the error
+        err => err.code === 'ENOENT' ? null : Promise.reject(err)
+    )
+}
