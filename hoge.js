62ce6f658a0ed2b9f6c67c2f9ad3b53119ba7d4a
yosukeN <yousken72@gmail.com>
2026-06-19 14:52:36 +0900

    add storages.

--- hoge.js
+++ hoge.js
@@ -1,3 +1,12 @@
 // const iso = require('isomorphitc-fetch');
 
 console.log(process.env.FOO)
+
+await dbRun(
+    `CREATE TABLE IF NOT EXISTS todo(
+        id TEXT PRIMARY KEY,
+        title TEXT NOT NULL,
+        completed BOOLEAN NOT NULL)
+    `
+)
+
--- readme.md
+++ readme.md
@@ -2,4 +2,8 @@
 `npm run start` : start express server.
 `npm run test` : run test scripts
 
+# how to run test script
+`npx mocha` : run test with mocha
+`npx jest`: run test with jest
+
 # Other
